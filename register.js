5ef098b66ef5901d6233c0a4c1f4bf86b9755554
mingueu <miguel.b01@aluno.ifsc.edu.br>
2025-09-24 13:49:26 -0300

    Update register.js

--- register.js
+++ register.js
@@ -1,12 +1,32 @@
-const form = document.getElementById('contact-form');
+const form = document.getElementById('reg_form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  if (!name || !email) {
-    alert('Please fill in all fields');
+  const mail = document.getElementById('mail').value;
+  const user = document.getElementById('user').value;
+  const passw = document.getElementById('passw').value;
+  const cpf = document.getElementById('cpf').value;
+  const bday = document.getElementById('bday').value;
+  if (!name) {
+    document.getElementById('al_name').innerHTML = 'Campo obrigatório';
+  } else if (!mail) {
+    document.getElementById('al_mail').innerHTML = 'Campo obrigatório';
+  } else if (!user) {
+    document.getElementById('al_user').innerHTML = 'Campo obrigatório';
+  } else if (!passw) {
+    document.getElementById('al_passw').innerHTML = 'Campo obrigatório';
+  } else if (!cpf) {
+    document.getElementById('al_cpf').innerHTML = 'Campo obrigatório';
+  } else if (!mail) {
+    document.getElementById('al_bday').innerHTML = 'Campo obrigatório';
   } else {
     // Form is valid, submit the form
+    document.getElementById('al_name').innerHTML = '';
+    document.getElementById('al_mail').innerHTML = '';
+    document.getElementById('al_user').innerHTML = '';
+    document.getElementById('al_passw').innerHTML = '';
+    document.getElementById('al_cpf').innerHTML = '';
+    document.getElementById('al_mail').innerHTML = '';
     form.submit();
   }
 });
