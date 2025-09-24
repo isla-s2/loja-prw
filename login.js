77aa6fc0ef7214cd0f29d05294ad0631f7ace191
mingueu <miguel.b01@aluno.ifsc.edu.br>
2025-09-24 13:42:26 -0300

    Update login.js

--- login.js
+++ login.js
@@ -2,8 +2,8 @@ const form = document.getElementById('login_form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const name = document.getElementById('user').value;
-  const email = document.getElementById('passw').value;
-  if (!name || !email) {
+  const passw = document.getElementById('passw').value;
+  if (!name || !passw) {
     alert('É necessário digitar usuário e senha para prosseguir');
   } else {
     // Form is valid, submit the form
